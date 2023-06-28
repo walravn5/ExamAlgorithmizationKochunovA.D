9c8d3e857524f76b56272a3f04078546e60b00ec
walravn5 <113089466+walravn5@users.noreply.github.com>
2023-06-28 10:18:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите вашу фамилию:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Введите желаемую оценку:");
+            string b = Console.ReadLine();
+
+            Console.WriteLine($"Поздравляю, {surname}! У вас автоматом {b} за экзамен");
+
+        }
+    }
+}
